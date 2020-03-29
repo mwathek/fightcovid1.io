73ed61c47df61cb8be3b7294ee9e1713ceffcb84
Wathek Matmati <wathek.matmati@gmail.com>
2020-03-30 00:21:25 +0100

    init fightcovid19

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Fight Covid 19 - Join the movement #FightCovid19</title>
+        <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="inner.css?v=1.2" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="icon" type="image/png" sizes="32x32" href="resources/media/logo_icon.png">
+<script defer src="https://friconix.com/cdn/friconix.js"> </script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+  <script src="resources/js/mwathek.js"></script>
+    </head>
+<body>
+<header>
+  <img src="logo.png" alt='Fight Covid-19 #FightCovid19'/>
+  <ul id='nav-links'>
+    <li>About</li>
+    <li>Contact</li>
+    <li>Tips</li>
+  </ul>
+  <ul id='act'>
+    <li><i class="fas fa-heartbeat"></i> Sign Up</li>
+    <li id='login'><i class="fas fa-long-arrow-alt-right"></i> Login</li>
+  </ul>
+</header>
+<div id="container">
+
+<div class='hm-cnt'>
+  <h1 class='logo-des'>
+    <span id='p1'>Fight</span><span id='p2'>Covid19</span>
+    </h1>
+    <p>Join the Movement <span style='color:#00acee'>#FightCovid19</span><br> <span class='smlr'>Help others and get the help you need.
+    You are NOT alone in this crisis! Despite the distance we can remain a strong united community.</span></p>
+    <h2>Please do NOT touch your face unless you know your hands are REALLY clean. #StaySafe #InQuarantine</h2>
+    <ul id="ftr">
+      <li><i class="fas fa-check"></i> Help nurses and health care providers</li>
+      <li><i class="fas fa-check"></i> Find local help in your community</li>
+      <li><i class="fas fa-check"></i> Let people know what you need</li>
+      <li><i class="fas fa-check"></i >Help your community get through</li>
+</ul>
+    </ul>
+    <form id='signup'>
+      <h3>It's <span>FREE</span> and <span>Awesome</span> to do good</h3>
+      <input id='nm' name='nm' placeholder='Full Name...' required/>
+      <input id='un' name='un' placeholder='@username...' required/>
+      <input id='adr' name='adr' placeholder='Address...' required/>
+      <input name='ue' placeholder='@ Email...' required/>
+      <button><i class="far fa-arrow-alt-circle-right"></i> Join</button>
+
+    </form>
+    <div class='hm-info'>
+      <h5>We are not taking any donations at this time. To help us, please share this with your friends family or invite them to like our page on Facebook <a href='https://fb.com/fightcovid19.io/' target="_BLANK">fb.com/fightcovid19.io/</a></h5>
+      <p>This is not government owned and doesn't belong to any health care related agency. It is funded and created by another person, just like you a software engineer whom also lost his job/oportunities following social distancing and self-quarantine. I hope it helps us unite despite the distance as we face this epidemic and that we"ll be generous and considerate to help those of us in need. 
+        <br>I wish you all well! #FightCovid19 #WeGotThis #CoronaVirus</p>
+</div>
+</div>
+
+</div>
+<footer>
+  <p>&copy; FightCovid19.io</p>
+  <ul id='so'>
+    <li id='tw'><i class="fab fa-twitter"></i></li>
+    <li id='fb'><i class="fab fa-facebook-f"></i></li>
+  </ul>
+</footer>
+
+</body>
+</html>
