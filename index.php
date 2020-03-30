6cccae58e0dde88c5b86be53f223a0d7f131ba1b
Wathek Matmati <wathek.matmati@gmail.com>
2020-03-31 00:07:40 +0100

    init act setup

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>FightCovid19 - Join the movement #FightCovid19</title>
+        <title>FightCovid19 - Join the movement #FightCovid19 Support your community in fighting the Corona Virtus.</title>
         <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link href="inner.css?v=1.2" rel="stylesheet">
@@ -38,28 +38,33 @@
     <h2><i class="fas fa-info-circle"></i> Important Reminder: Please do NOT touch your face unless you know your hands are REALLY clean. #StaySafe #BeProtected.</h2>
     <ul id="ftr">
       <li><i class="fas fa-heart"></i> Help nurses, elderly, care-givers..etc.</li>
-      <li><i class="fas fa-smile"></i> Stay onnected beyond the distance</li>
+      <li><i class="fas fa-smile"></i> Stay connected beyond the distance</li>
       <li><i class="fas fa-search"></i> Find local support in your community</li>
       <li><i class="fas fa-ambulance"></i> Offer help, delivery, donations...etc.</li>
    
 </ul>
     </ul>
     <form id='signup' method='POST' autocomplete="off">
-      <h3>Every Act of kindness <span>Counts</span><br>
+      <h3><span style='font-size:12px;'><i class="fas fa-heart"></i></span> Every Act of Kindness <span>Counts</span><br>
     <span></h3>
       <input id='nm' name='nm' placeholder='Full Name...' required/>
       <input id='un' name='un' placeholder='@username...' required/>
       <input type='email' name='ue' placeholder='@ Email...' required/>
       <input type='password' id='up' name='uc' placeholder='Set a Password...' required/>
       <input type='password' id='uc' name='uz' placeholder='Confirm Password...' required/>
-      <button><i class="far fa-arrow-alt-circle-right"></i> Join Us</button>
+      <button><i class="far fa-heart"></i> Join Us</button>
 
     </form>
     <div class='hm-info'>
       <h5>We are not taking any donations at this time. To help us, please share this with your friends family or invite them to like our page on Facebook 
         <a href='https://fb.com/fightcovid19.io/' target="_BLANK">fb.com/FightCovid19.io/</a></h5>
-      <p>This is not government owned and doesn't belong to any health care related agency. It is funded and created by another person, just like you a software engineer whom also lost his job/oportunities following social distancing and self-quarantine. I hope it helps us unite despite the distance as we face this epidemic and that we"ll be generous and considerate to help those of us in need. 
-        <br>I wish you all well! #FightCovid19 #WeGotThis #CoronaVirus</p>
+      <p>This application/platoform is totally FREE and built on the best intentions for the people. 
+        FightCovid19.io not government owned and doesn't belong to any health related agency. 
+        It is simply funded and developed by another person just like you, whom also lost 
+        his job/oportunities following social distancing and self-quarantine. 
+        I hope it helps us unite despite the distance as we face this epidemic and that we"ll be generous 
+        and considerate to help those of us in need. 
+        I wish you all well! #FightCovid19 #WeGotThis #DefeatCoronaVirus</p>
 </div>
 </div>
 
